470584737ba3c8d21539ef03fc9371c73da1c229
Ameri <mohammedagl6@gmail.com>
2021-07-12 11:35:49 +0200

    Ameri node week2

--- week1/homework/hackyourtemperature/server.js
+++ week1/homework/hackyourtemperature/server.js
@@ -1,10 +1,23 @@
-const express = require('express');
+const express = require('express')
 const app = express();
+const exphbs  = require('express-handlebars');
+
+app.set('view engine', 'handlebars');
+app.engine('handlebars', exphbs({ defaultLayout: false }));
+
 
 const port = 3000;
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.get('/', (req, res) => {
-    res.send('hello from backend to frontend!');
+    res.render('index');
+});
+
+app.post('/weather', (req, res) => {
+    const cityName = req.body.cityName;
+    res.send(cityName);
 });
 
 app.listen(port, () => {
